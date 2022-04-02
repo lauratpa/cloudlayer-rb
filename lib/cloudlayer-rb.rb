0ba109acdfad23f4237d077d687be1d336bc1892
Laura Paakkinen <la.paakkinen@gmail.com>
2022-04-02 10:43:40 +0200

    Initial commit

---
+++ lib/cloudlayer-rb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cloudlayer/version"
+
+module Cloudlayer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cloudlayer_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Cloudlayer do
+  it "has a version number" do
+    expect(Cloudlayer::VERSION).not_to be nil
+  end
+end
